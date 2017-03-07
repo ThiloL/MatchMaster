bddd343665cf8d6fefa1c7bc245a660d163f49d0
Thilo <login@thilo-langbein.de>
2017-03-07 18:19:13 +0100

    Updsate 1

---
+++ MatchMaster/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace MatchMaster
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        static App()
+        {
+            FrameworkElement.LanguageProperty.OverrideMetadata(
+                typeof(FrameworkElement),
+                new FrameworkPropertyMetadata(
+                    XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+        }
+    }
+}
---
+++ MatchMaster/Match.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MatchMaster
+{
+    /// <summary>
+    /// Interaction logic for Match.xaml
+    /// </summary>
+    public partial class MatchWindow : Window
+    {
+        public MatchWindow()
+        {
+            InitializeComponent();
+            FillGrid();
+        }
+
+        private void FillGrid()
+        {
+            using (SqlConnection con = new SqlConnection(Properties.Settings.Default.MMConnectionString))
+            {
+                const string s = "select iddesc as ID, title as Title, [start] as [Start], [end] as [End] from match ORDER BY [ID] desc";
+                SqlCommand c = new SqlCommand(s, con);
+                SqlDataAdapter da = new SqlDataAdapter(c);
+                DataTable dt = new DataTable("Match");
+                da.Fill(dt);
+                GvMatch.ItemsSource = dt.DefaultView;
+            }
+        }
+    }
+}
